a3e259d6edd466311a02e3d4d35bbc4e1bc4e7a9
Lila Ouadi <lilaouadi@Lilas-MacBook-Air.local>
2019-10-21 19:47:43 -0700

    added html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,96 +1,150 @@
-$( document ).ready(function(){
-    var Random=Math.floor(Math.random()*101+19)
-    // Selects a random number to be shown at the start of the game
-    // Number should be should be between 19 - 120
-    //
-    $('#randomNumber').text(Random);
-    // Appending random number to the randomNumber id in the html doc
-    //
-    var num1= Math.floor(Math.random()*11+1)
-    var num2= Math.floor(Math.random()*11+1)
-    var num3= Math.floor(Math.random()*11+1)
-    var num4= Math.floor(Math.random()*11+1)
-    // Setting up random numbers for each jewel
-    // Random number has to be between 1 - 12
-    // 
-    var userTotal= 0; 
-    var wins= 0;
-    var losses = 0;
-    //  Decaring variables for tallies
-  $('#numberWins').text(wins);
-  $('#numberLosses').text(losses);
-  //resets the game
-  function reset(){
-        Random=Math.floor(Math.random()*101+19);
-        console.log(Random)
-        $('#randomNumber').text(Random);
-        num1= Math.floor(Math.random()*11+1);
-        num2= Math.floor(Math.random()*11+1);
-        num3= Math.floor(Math.random()*11+1);
-        num4= Math.floor(Math.random()*11+1);
-        userTotal= 0;
-        $('#finalTotal').text(userTotal);
-        } 
-  //adds the wins to the userTotal
-  function yay(){
-  alert("You won!");
-    wins++; 
-    $('#numberWins').text(wins);
-    reset();
-  }
-  //addes the losses to the userTotal
-  function loser(){
-  alert ("You lose!");
-    losses++;
-    $('#numberLosses').text(losses);
-    reset()
-  }
-  //sets up click for jewels
-    $('#one').on ('click', function(){
-      userTotal = userTotal + num1;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal); 
-            //sets win/lose conditions
-          if (userTotal == Random){
-            yay();
-          }
-          else if ( userTotal > Random){
-            loser();
-          }   
-    })  
-    $('#two').on ('click', function(){
-      userTotal = userTotal + num2;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal); 
-          if (userTotal == Random){
-            yay();
-          }
-          else if ( userTotal > Random){
-            loser();
-          } 
-    })  
-    $('#three').on ('click', function(){
-      userTotal = userTotal + num3;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal);
-  //sets win/lose conditions
-            if (userTotal == Random){
-            yay();
-          }
-          else if ( userTotal > Random){
-            loser();
-          } 
-    })  
-    $('#four').on ('click', function(){
-      userTotal = userTotal + num4;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal); 
+var userTotal = 0;
+    var winTotal = 0;
+    var loseTotal = 0;
+    var crystal1 = Math.floor(Math.random()*12)+1;
+    var crystal2 = Math.floor(Math.random()*12)+1;
+    var crystal3 = Math.floor(Math.random()*12)+1;
+    var crystal4 = Math.floor(Math.random()*12)+1;
+    var guessNumber = Math.floor(Math.random()*102)+19;
+    
+    $('#crystalOne').on('click', function(){
+        //we need to grab crystal1 value and add to total
+        userTotal = userTotal + crystal1;
+    $('#scoreArea').html(userTotal);
+    
+    if (userTotal === guessNumber) {
+        winTotal = winTotal + 1;
+        $('#gameWinCount').html('');
+        $('#gameWinCount').html('Wins:'+winTotal);
+        crystal1 = Math.floor(Math.random()*12)+1;
+        crystal2 = Math.floor(Math.random()*12)+1;
+        crystal3 = Math.floor(Math.random()*12)+1;
+        crystal4 = Math.floor(Math.random()*12)+1;
+        guessNumber = Math.floor(Math.random()*102)+19;
+        $("#guessNumber").html(guessNumber);
+        userTotal=0;
+        $('#scoreArea').html(userTotal);
+    
+    }
+    else if (userTotal > guessNumber) {
+        loseTotal = loseTotal + 1;
+        $('#gameLossCount').html('');
+        $('#gameLossCount').html('Losses:'+loseTotal);
+        crystal1 = Math.floor(Math.random()*12)+1;
+        crystal2 = Math.floor(Math.random()*12)+1;
+        crystal3 = Math.floor(Math.random()*12)+1;
+        crystal4 = Math.floor(Math.random()*12)+1;
+        guessNumber = Math.floor(Math.random()*102)+19;
+        $("#guessNumber").html(guessNumber);
+        userTotal=0;
+        $('#scoreArea').html(userTotal);
         
-            if (userTotal == Random){
-            yay();
-          }
-          else if ( userTotal > Random){
-            loser();
-          }
-    });   
-  }); 
+    }  
+    });
+
+    $('#crystaltwo').on('click', function(){
+        //we need to grab crystal1 value and add to total
+        userTotal = userTotal + crystal2;
+    $('#scoreArea').html(userTotal);
+    
+    if (userTotal === guessNumber) {
+        winTotal = winTotal + 1;
+        $('#gameWinCount').html('');
+        $('#gameWinCount').html('Wins:'+winTotal);
+        crystal1 = Math.floor(Math.random()*12)+1;
+        crystal2 = Math.floor(Math.random()*12)+1;
+        crystal3 = Math.floor(Math.random()*12)+1;
+        crystal4 = Math.floor(Math.random()*12)+1;
+        guessNumber = Math.floor(Math.random()*102)+19;
+        $("#guessNumber").html(guessNumber);
+        userTotal=0;
+        $('#scoreArea').html(userTotal);
+    
+    }
+    else if (userTotal > guessNumber) {
+        loseTotal = loseTotal + 1;
+        $('#gameLossCount').html('');
+        $('#gameLossCount').html('Losses:'+loseTotal);
+        crystal1 = Math.floor(Math.random()*12)+1;
+        crystal2 = Math.floor(Math.random()*12)+1;
+        crystal3 = Math.floor(Math.random()*12)+1;
+        crystal4 = Math.floor(Math.random()*12)+1;
+        guessNumber = Math.floor(Math.random()*102)+19;
+        $("#guessNumber").html(guessNumber);
+        userTotal=0;
+        $('#scoreArea').html(userTotal);
+        
+    }  
+    });
+
+    $('#crystalthree').on('click', function(){
+        //we need to grab crystal1 value and add to total
+        userTotal = userTotal + crystal3;
+    $('#scoreArea').html(userTotal);
+    
+    if (userTotal === guessNumber) {
+        winTotal = winTotal + 1;
+        $('#gameWinCount').html('');
+        $('#gameWinCount').html('Wins:'+winTotal);
+        crystal1 = Math.floor(Math.random()*12)+1;
+        crystal2 = Math.floor(Math.random()*12)+1;
+        crystal3 = Math.floor(Math.random()*12)+1;
+        crystal4 = Math.floor(Math.random()*12)+1;
+        guessNumber = Math.floor(Math.random()*102)+19;
+        $("#guessNumber").html(guessNumber);
+        userTotal=0;
+        $('#scoreArea').html(userTotal);
+    
+    }
+    else if (userTotal > guessNumber) {
+        loseTotal = loseTotal + 1;
+        $('#gameLossCount').html('');
+        $('#gameLossCount').html('Losses:'+loseTotal);
+        crystal1 = Math.floor(Math.random()*12)+1;
+        crystal2 = Math.floor(Math.random()*12)+1;
+        crystal3 = Math.floor(Math.random()*12)+1;
+        crystal4 = Math.floor(Math.random()*12)+1;
+        guessNumber = Math.floor(Math.random()*102)+19;
+        $("#guessNumber").html(guessNumber);
+        userTotal=0;
+        $('#scoreArea').html(userTotal);
+        
+    }  
+    });
+
+    $('#crystalfour').on('click', function(){
+        //we need to grab crystal1 value and add to total
+        userTotal = userTotal + crystal4;
+    $('#scoreArea').html(userTotal);
+    
+    if (userTotal === guessNumber) {
+        winTotal = winTotal + 1;
+        $('#gameWinCount').html('');
+        $('#gameWinCount').html('Wins:'+winTotal);
+        crystal1 = Math.floor(Math.random()*12)+1;
+        crystal2 = Math.floor(Math.random()*12)+1;
+        crystal3 = Math.floor(Math.random()*12)+1;
+        crystal4 = Math.floor(Math.random()*12)+1;
+        guessNumber = Math.floor(Math.random()*102)+19;
+        $("#guessNumber").html(guessNumber);
+        userTotal=0;
+        $('#scoreArea').html(userTotal);
+    
+    }
+    else if (userTotal > guessNumber) {
+        loseTotal = loseTotal + 1;
+        $('#gameLossCount').html('');
+        $('#gameLossCount').html('Losses:'+loseTotal);
+        crystal1 = Math.floor(Math.random()*12)+1;
+        crystal2 = Math.floor(Math.random()*12)+1;
+        crystal3 = Math.floor(Math.random()*12)+1;
+        crystal4 = Math.floor(Math.random()*12)+1;
+        guessNumber = Math.floor(Math.random()*102)+19;
+        $("#guessNumber").html(guessNumber);
+        userTotal=0;
+        $('#scoreArea').html(userTotal);
+        
+    }  
+    });
+   //$("#R1").html(crystalNumber);
+   $("#guessNumber").html(guessNumber);
